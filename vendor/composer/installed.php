2913132554a7dff4a55f03e655307f06d576992d
lukifi1 <93972165+lukifi1@users.noreply.github.com>
2022-04-18 21:53:34 +0200

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'htlw3r/viewa',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'htlw3r/viewa' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'typo3fluid/fluid' => array(
+            'pretty_version' => '2.7.1',
+            'version' => '2.7.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../typo3fluid/fluid',
+            'aliases' => array(),
+            'reference' => 'a60a9219ab556104c70abb3efad45caa2c188aa5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
